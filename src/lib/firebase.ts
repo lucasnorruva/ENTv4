c4d9282ea4d6dc5c4a55f19dd245affc161525a9
John Charles Lidman <john.charles@norruva.com>
2025-06-29 15:19:51 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,8 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = process.env.FIREBASE_CONFIG
-  ? JSON.parse(process.env.FIREBASE_CONFIG)
+const firebaseConfig = process.env.NEXT_PUBLIC_FIREBASE_CONFIG
+  ? JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG)
   : {};
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
