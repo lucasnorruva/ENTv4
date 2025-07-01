c58de4318f0e72a3f4342b584a9c6957dfb49c73
John Charles Lidman <john.charles@norruva.com>
2025-07-01 03:33:32 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -20,6 +20,12 @@ const nextConfig = {
   },
   webpack(config) {
     config.experiments = { ...config.experiments, asyncWebAssembly: true };
+    
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: "webassembly/async",
+    });
+
     return config;
   },
 };
