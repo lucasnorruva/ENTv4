a330475e5dddb193265172301758886f37da649c
John Charles Lidman <john.charles@norruva.com>
2025-07-02 20:27:05 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -1,33 +1,39 @@
 "use client";
 
 import * as React from "react";
-import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area";
+import {
+  Root as ScrollAreaPrimitiveRoot,
+  Viewport as ScrollAreaPrimitiveViewport,
+  Scrollbar as ScrollAreaPrimitiveScrollbar,
+  Thumb as ScrollAreaPrimitiveThumb,
+  Corner as ScrollAreaPrimitiveCorner,
+} from "@radix-ui/react-scroll-area";
 
 import { cn } from "@/lib/utils";
 
 const ScrollArea = React.forwardRef<
-  React.ElementRef<typeof ScrollAreaPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
+  React.ElementRef<typeof ScrollAreaPrimitiveRoot>,
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitiveRoot>
 >(({ className, children, ...props }, ref) => (
-  <ScrollAreaPrimitive.Root
+  <ScrollAreaPrimitiveRoot
     ref={ref}
     className={cn("relative overflow-hidden", className)}
     {...props}
   >
-    <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
+    <ScrollAreaPrimitiveViewport className="h-full w-full rounded-[inherit]">
       {children}
-    </ScrollAreaPrimitive.Viewport>
+    </ScrollAreaPrimitiveViewport>
     <ScrollBar />
-    <ScrollAreaPrimitive.Corner />
-  </ScrollAreaPrimitive.Root>
+    <ScrollAreaPrimitiveCorner />
+  </ScrollAreaPrimitiveRoot>
 ));
-ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName;
+ScrollArea.displayName = ScrollAreaPrimitiveRoot.displayName;
 
 const ScrollBar = React.forwardRef<
-  React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>
+  React.ElementRef<typeof ScrollAreaPrimitiveScrollbar>,
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitiveScrollbar>
 >(({ className, orientation = "vertical", ...props }, ref) => (
-  <ScrollAreaPrimitive.ScrollAreaScrollbar
+  <ScrollAreaPrimitiveScrollbar
     ref={ref}
     orientation={orientation}
     className={cn(
@@ -40,9 +46,9 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
-  </ScrollAreaPrimitive.ScrollAreaScrollbar>
+    <ScrollAreaPrimitiveThumb className="relative flex-1 rounded-full bg-border" />
+  </ScrollAreaPrimitiveScrollbar>
 ));
-ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
+ScrollBar.displayName = ScrollAreaPrimitiveScrollbar.displayName;
 
 export { ScrollArea, ScrollBar };
