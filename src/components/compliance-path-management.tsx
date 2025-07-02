e56e6d1e52ff0fea3d7eb4172a95d78c56de591a
John Charles Lidman <john.charles@norruva.com>
2025-07-02 04:11:42 +0000

    ReferenceError: AlertDialogTrigger is not defined     at eval (webpack-i

--- src/components/compliance-path-management.tsx
+++ src/components/compliance-path-management.tsx
@@ -49,6 +49,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import CompliancePathForm from './compliance-path-form';
 
--- src/components/product-table.tsx
+++ src/components/product-table.tsx
@@ -57,6 +57,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
