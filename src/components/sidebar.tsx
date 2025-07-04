de85456ce62b84822429750ad97052d305306fe2
John Charles Lidman <john.charles@norruva.com>
2025-07-04 02:19:26 +0000

    next task

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,7 @@
+// This component is obsolete and has been replaced by the more advanced
+// set of components in /components/ui/sidebar.tsx and /components/dashboard-sidebar.tsx.
+// This file can be removed in a future cleanup.
+
+export default function Sidebar() {
+  return null;
+}
